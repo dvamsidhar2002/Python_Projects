5abc08ca86f77d072cd2c7da8ce7c75e80515b0b
D.Vamsidhar <83284121+Vamsi4861@users.noreply.github.com>
2021-12-22 14:56:35 +0530

    Update text_to_audio.py

--- text_to_audio.py
+++ text_to_audio.py
@@ -1,8 +1,10 @@
-from gtts import gTTS
+from gtts import gTTS   /*gtts - google text to speech services*/
 import os
 input=open('1.txt')
 r=input.read()
-language = 'en'
+
+language = 'en'  // language -> English
+
 audio=gTTS(text=r,lang=language,slow=False)
 audio.save("output.wav")
 os.system("output.wav")
